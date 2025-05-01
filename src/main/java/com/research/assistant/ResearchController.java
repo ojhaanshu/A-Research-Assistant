d8a408454f30779f6162815086c26f177ad354fd
Anshu Ojha <oanshu600@gmail.com>
2025-05-01 17:44:21 +0530

    first commit

---
+++ src/main/java/com/research/assistant/ResearchController.java
@@ -0,0 +1,27 @@
+package com.research.assistant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/research")
+@CrossOrigin(origins="*")
+@AllArgsConstructor
+public class ResearchController {
+	@Autowired
+	private final ResearchService researchService;
+	
+	@PostMapping("/process")
+	public ResponseEntity<String> processContent(@RequestBody ResearchRequest request ){
+		String result = researchService.processContent(request);
+		return ResponseEntity.ok(result);
+	}
+
+}
---
+++ src/main/java/com/research/assistant/ResearchService.java
@@ -0,0 +1,93 @@
+package com.research.assistant;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class ResearchService {
+	@Value("${gemini.api.url}")
+	private String geminiApiUrl;
+	@Value("${gemini.api.key}")
+	private String geminiApiKey;
+	
+	private final WebClient webClient;
+	private final ObjectMapper objectMapper;
+	
+	public ResearchService(WebClient.Builder webClientBuilder,ObjectMapper objectMapper){
+		this.webClient = webClientBuilder.build();
+		this.objectMapper= objectMapper;
+		
+		
+	}
+
+	public String processContent(ResearchRequest request) {
+		//Build the prompt
+		String prompt = buildPrompt(request);
+		//Query the Al Api
+		Map<String,Object> requestBody = Map.of(
+				"contents",new Object[] {
+						Map.of("parts",new Object[] {
+						      Map.of("text",prompt)
+						})});
+		//Prepare POST request → Set full URL → Attach request body → Send the request → Wait for response → Get response as String
+
+		String response = webClient.post()
+				          .uri(geminiApiUrl+geminiApiKey)
+				          .bodyValue(requestBody)
+				          .retrieve()
+				          .bodyToMono(String.class)
+				          .block();
+		//Parse the response
+		
+		//Return response
+		return extractTextFromResponse(response);
+	}
+	
+	private String extractTextFromResponse(String response) {
+		try {
+			GeminiResponse geminiResponse = objectMapper.readValue(response, GeminiResponse.class);
+			if(geminiResponse .getCandidates()!=null && !geminiResponse.getCandidates().isEmpty() ) {
+				GeminiResponse.Candidate firstCandidate = geminiResponse.getCandidates().get(0);
+				if(firstCandidate.getContent() != null && 
+				   firstCandidate.getContent().getParts()!=null &&
+				   !firstCandidate.getContent().getParts().isEmpty()) {
+					return firstCandidate.getContent().getParts().get(0).getText();
+					
+				}
+			}
+			
+		}
+		
+		catch(Exception E) {
+			System.out.println("parsing Error " +E.getMessage());
+			
+		}
+		return "No Content found in response";
+		
+		
+	}
+	
+	private String buildPrompt(ResearchRequest request) {
+		StringBuilder prompt = new StringBuilder();
+		switch(request.getOperation()){
+		case "summarize":
+			prompt.append("Provide a clear and concise summary of the following text in a few sentences:\n\n");
+			break;
+		case "suggest":
+			prompt.append("Based on a following content:suggest related topics and further reading. Format the response with clear headings and bullet points:\n\n ");
+		    break;
+		default:
+			throw new IllegalArgumentException( "Unknown Operation: " +  request.getOperation());
+		}
+		prompt.append(request.getContent());
+		return prompt.toString();
+		
+		
+	}
+
+}
